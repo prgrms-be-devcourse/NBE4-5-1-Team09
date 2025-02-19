115e51ae3f290d80d53732cce9bfc068566b323c
choijaewoo <cchoi00332244@gmail.com>
2025-02-19 15:58:56 +0900

    feat: 장바구니 기능을 위한 cart, cartitem entity 추가.

---
+++ backend/src/main/java/com/example/cafe/domain/trade/repository/CartRepository.java
@@ -0,0 +1,7 @@
+package com.example.cafe.domain.trade.repository;
+
+import com.example.cafe.domain.trade.domain.entity.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartRepository extends JpaRepository<Cart, Long> {
+}
