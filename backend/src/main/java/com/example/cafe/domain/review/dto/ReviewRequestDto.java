f0a73ef90e633829642d0ececb58bb5a76b16827
SDHSeoulTech <dongseoultech@gmail.com>
2025-02-19 15:45:49 +0900

    feat: ReviewRequestDto 작성 완료
    리뷰 작성을 요청받을 때 사용하는 dto

---
+++ backend/src/main/java/com/example/cafe/domain/review/dto/ReviewRequestDto.java
@@ -0,0 +1,13 @@
+package com.example.cafe.domain.review.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ReviewRequestDto {
+    private Long memberId;         // 작성자의 멤버 ID
+    private Long itemId;           // 리뷰가 작성될 상품 ID
+    private String reviewContent;  // 리뷰 내용
+    private double rating;         // 리뷰 평점
+}
