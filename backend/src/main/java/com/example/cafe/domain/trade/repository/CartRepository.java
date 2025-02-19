58cbcf53295a3404f7c031e5fc3e0da36a77d07c
choijaewoo <cchoi00332244@gmail.com>
2025-02-19 17:27:23 +0900

    feat: 장바구니 아이템 추가기능 구현

--- backend/src/main/java/com/example/cafe/domain/trade/repository/CartRepository.java
+++ backend/src/main/java/com/example/cafe/domain/trade/repository/CartRepository.java
@@ -3,5 +3,8 @@ package com.example.cafe.domain.trade.repository;
 import com.example.cafe.domain.trade.domain.entity.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CartRepository extends JpaRepository<Cart, Long> {
+    Optional<Cart> findByMemberId(Long memberId);
 }
