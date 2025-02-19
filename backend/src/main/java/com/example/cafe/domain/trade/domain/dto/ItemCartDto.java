e04c89c164a7a855829540152c4b367ffa9f6b21
choijaewoo <cchoi00332244@gmail.com>
2025-02-19 17:51:57 +0900

    feat: 장바구니에 들어있는 상품 수량 변경 및 삭제 기능 구현

--- backend/src/main/java/com/example/cafe/domain/trade/domain/dto/ItemCartDto.java
+++ backend/src/main/java/com/example/cafe/domain/trade/domain/dto/ItemCartDto.java
@@ -3,7 +3,7 @@ package com.example.cafe.domain.trade.domain.dto;
 import lombok.Data;
 
 @Data
-public class ItemAddCartDto {
+public class ItemCartDto {
     private Long memberId;
     private Long itemId;
     private int quantity;
