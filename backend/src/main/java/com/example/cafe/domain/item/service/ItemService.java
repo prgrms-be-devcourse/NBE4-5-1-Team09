8bdec2d078936e3431cc6f200e50fb76c5e5b0a1
ShinYounHo <suh2963@gmail.com>
2025-02-19 16:41:22 +0900

    feat: getAllItems, getItem 구현

--- backend/src/main/java/com/example/cafe/domain/item/service/ItemService.java
+++ backend/src/main/java/com/example/cafe/domain/item/service/ItemService.java
@@ -1,12 +1,23 @@
 package com.example.cafe.domain.item.service;
 
+import com.example.cafe.domain.item.entity.Item;
 import com.example.cafe.domain.item.respository.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ItemService {
 
     private final ItemRepository itemRepository;
+
+    public List<Item> getAllItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item getItem(Long id) {
+        return itemRepository.findById(id).get();
+    }
 }
