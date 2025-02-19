8143e69de8c1d6bd0df929cc7f749c5a0f6c21af
choijaewoo <cchoi00332244@gmail.com>
2025-02-19 17:51:57 +0900

    feat: 장바구니에 들어있는 상품 수량 변경 및 삭제 기능 구현

--- backend/src/main/java/com/example/cafe/domain/trade/service/user/UserCartService.java
+++ backend/src/main/java/com/example/cafe/domain/trade/service/user/UserCartService.java
@@ -2,7 +2,7 @@ package com.example.cafe.domain.trade.service.user;
 
 import com.example.cafe.domain.item.entity.Item;
 import com.example.cafe.domain.item.respository.ItemRepository;
-import com.example.cafe.domain.trade.domain.dto.ItemAddCartDto;
+import com.example.cafe.domain.trade.domain.dto.ItemCartDto;
 import com.example.cafe.domain.trade.domain.entity.Cart;
 import com.example.cafe.domain.trade.domain.entity.CartItem;
 import com.example.cafe.domain.trade.repository.CartItemRepository;
@@ -11,6 +11,7 @@ import com.example.cafe.domain.trade.repository.TradeItemRepository;
 import com.example.cafe.domain.trade.repository.TradeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -22,12 +23,9 @@ public class UserCartService {
     private final ItemRepository itemRepository;
 
 
-    public void addItemToCart(ItemAddCartDto addItem) {
-        /**
-         * addItem 으로 해당 유저의 장바구니에 아이템을 추가하는 로직.
-         */
-
-        Cart cart = cartRepository.findByMemberId(addItem.getMemberId()).orElseThrow(() -> new RuntimeException("유저 [" + addItem.getMemberId() + "]의 카트를 찾을 수 없습니다."));
+    @Transactional
+    public void addItemToCart(ItemCartDto addItem) {
+        Cart cart = getCart(addItem);
 
         Item item = itemRepository.findById(addItem.getItemId()).orElseThrow(() -> new RuntimeException("해당 아이템을 찾을 수 없어 카트에 추가하지 못하였습니다."));
 
@@ -47,5 +45,31 @@ public class UserCartService {
         }
     }
 
-    
+    @Transactional
+    public void QuantityItemToCart(ItemCartDto editItem) {
+        Cart cart = getCart(editItem);
+
+        Item item = itemRepository.findById(editItem.getItemId()).orElseThrow(() -> new RuntimeException("해당 아이템을 찾을 수 없어 카트에 추가하지 못하였습니다."));
+
+        CartItem cartItem = cart.getCartItems().stream().filter(cartAddItem -> cartAddItem.getItem().getId().equals(item.getId())).findFirst().orElse(null);
+
+        if (cartItem == null) {
+            throw new RuntimeException("해당 상품이 카트에 존재하지 않아 수량을 수정에 실패하였습니다.");
+        }
+
+        if (cartItem.getQuantity() < editItem.getQuantity()) {
+            throw new RuntimeException("장바구니에 있는 수량보다 취소 수량이 더 많습니다.");
+        }
+        //삭제 요청 수량이 장바구니 수량과 동일하거나, 상품 삭제 요청. 수량 감소 요청 숫자가 0이면 삭제요청으로 간주.
+        else if (cartItem.getQuantity() == editItem.getQuantity() || editItem.getQuantity() == 0) {
+            cartItemRepository.delete(cartItem);
+        }
+        else {
+            cartItem.setQuantity(cartItem.getQuantity() - editItem.getQuantity());
+        }
+    }
+
+    private Cart getCart(ItemCartDto itemCartDto) {
+        return cartRepository.findByMemberId(itemCartDto.getMemberId()).orElseThrow(() -> new RuntimeException("유저 [" + itemCartDto.getMemberId() + "]의 카트를 찾을 수 없습니다."));
+    }
 }
