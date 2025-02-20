b0fe4c6d332d14345855879cfae5b9d9e46f8ad1
ShinYounHo <suh2963@gmail.com>
2025-02-20 12:35:43 +0900

    feat: imgae 업로드 기능 추가

--- backend/src/main/java/com/example/cafe/domain/item/controller/ItemController.java
+++ backend/src/main/java/com/example/cafe/domain/item/controller/ItemController.java
@@ -3,18 +3,25 @@ package com.example.cafe.domain.item.controller;
 import com.example.cafe.domain.item.dto.ItemRequestDto;
 import com.example.cafe.domain.item.dto.ItemResponseDto;
 import com.example.cafe.domain.item.service.ItemService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
 @RequestMapping("/items")
 @RequiredArgsConstructor
 public class ItemController {
+
+    @Value("${file.upload-dir}")
+    private String uploadDir;
 
     private final ItemService itemService;
 
@@ -29,10 +36,16 @@ public class ItemController {
     }
 
     @PostMapping
-    public ResponseEntity<ItemResponseDto> createItem(@RequestBody @Valid ItemRequestDto itemRequestDto) {
+    public ResponseEntity<ItemResponseDto> createItem(@RequestPart("item") String item,
+                                                      @RequestPart("image") MultipartFile imageFile) throws IOException {
 
-        ItemResponseDto item = itemService.createItem(itemRequestDto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(item);
+        // item을 ItemRequestDto로 변환
+        ObjectMapper objectMapper = new ObjectMapper();
+        ItemRequestDto itemRequestDto = objectMapper.readValue(item, ItemRequestDto.class);
+
+        // 아이템 생성
+        ItemResponseDto savedItem = itemService.createItem(itemRequestDto, imageFile);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedItem);
     }
 
     @PutMapping("/{id}")
@@ -46,5 +59,4 @@ public class ItemController {
         itemService.deleteItem(id);
         return ResponseEntity.noContent().build();
     }
-
 }
