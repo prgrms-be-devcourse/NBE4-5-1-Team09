72589426a26de711210fc79ed2ae85c11236b10b
choijaewoo <cchoi00332244@gmail.com>
2025-02-21 16:34:05 +0900

    feat: 장바구니 추가, 수량 수정 기능

--- backend/src/main/java/com/example/cafe/domain/trade/service/user/UserCartService.java
+++ backend/src/main/java/com/example/cafe/domain/trade/service/user/UserCartService.java
@@ -20,6 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static com.example.cafe.domain.trade.domain.dto.response.CartListResponseDto.*;
