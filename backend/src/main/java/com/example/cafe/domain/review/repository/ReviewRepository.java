8ec2be608f6992ad05b0fadd73a21a8007241f9a
SDHSeoulTech <dongseoultech@gmail.com>
2025-02-21 15:51:24 +0900

    fix: 커스텀 쿼리 문 추가
    JPA에서는 List만 반환해주므로 Double을 반환받기 위해 repository에 커스텀 쿼리문을 추가해서 미리 계산을 해 Double을 반환하게 함

--- backend/src/main/java/com/example/cafe/domain/review/repository/ReviewRepository.java
+++ backend/src/main/java/com/example/cafe/domain/review/repository/ReviewRepository.java
@@ -1,7 +1,9 @@
 package com.example.cafe.domain.review.repository;
 
 import com.example.cafe.domain.review.entity.Review;
+import jakarta.persistence.Tuple;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +13,6 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
     List<Review> findByItem_IdOrderByCreatedAtDesc(Long itemId); // 최신순
     List<Review> findByItem_IdOrderByRatingDesc(Long itemId);    // 평점 높은 순
     List<Review> findByItem_IdOrderByRatingAsc(Long itemId);     // 평점 낮은 순
+    @Query("SELECT AVG(r.rating) FROM Review r WHERE r.item.id = :itemId")
     Double findAverageRatingByItem_Id(Long itemId);
 }
