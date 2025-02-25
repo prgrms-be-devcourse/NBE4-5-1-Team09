8dbe961b76c7cf77688639a436efe26e6df2e73b
Choi Jaewoo <51539046+cjw0324@users.noreply.github.com>
2025-02-25 17:47:09 +0900

    Feat/#78 feat trade pay (#87)
    * feat: spring scheduler 적용하여 오후 2시 일괄 PREPARE_DELIVERY -> BEFORE_DELIVERY
    
    * fix: spring scheduler 적용하여 오후 2시 일괄 PREPARE_DELIVERY -> BEFORE_DELIVERY
    
    * fix: spring scheduler 적용하여 오후 2시 일괄 PREPARE_DELIVERY -> BEFORE_DELIVERY

--- backend/src/main/java/com/example/cafe/CafeApplication.java
+++ backend/src/main/java/com/example/cafe/CafeApplication.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SecurityScheme(
 		name = "bearerAuth",
@@ -14,6 +15,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 )
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableScheduling
 public class CafeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CafeApplication.class, args);
