0fb1a99a6f9468dde6f2b73235e89379e5e811e5
wkdan <jmy8876@gmail.com>
2025-02-25 11:28:12 +0900

    feat: db 설정

--- backend/src/main/java/com/example/cafe/global/config/SecurityConfig.java
+++ backend/src/main/java/com/example/cafe/global/config/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         // 허용할 출처를 명시합니다.
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3001"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
